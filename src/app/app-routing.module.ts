4de0f39167a6ff2003e5a0dec66411bc01b109f1
ThomasChen94 <ThomasChen@Thomas-Work-Station.local>
2019-03-27 10:06:06 -0700

    may replace the main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component'
 import { MainInfoPageComponent } from './main-info-page/main-info-page.component'
+import { IndexPageComponent } from './index-page/index-page.component'
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
-  { path: 'home', component: MainInfoPageComponent},
+  { path: 'home', component: MainInfoPageComponent },
+  { path: 'index', component: IndexPageComponent },
   { path: '',
     redirectTo: '/login',
     pathMatch: 'full'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { MainInfoPageComponent } from './main-info-page/main-info-page.component';
+import { IndexPageComponent } from './index-page/index-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    MainInfoPageComponent
+    MainInfoPageComponent,
+    IndexPageComponent
   ],
   imports: [
     BrowserModule,
